cb47d1ed01b8781b516e461ccc301013efe4ae30
Kevin Radtke <kevinradtkeg@gmail.com>
2019-11-09 15:16:10 -0600

    Refactor: Move test output to a testLog module

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,7 +1,21 @@
 import pprint
-import parser
+from parser import parser
 import symbol_table
 import sys
+import code_generator
+
+def testLog():
+    '''Prints stuff for testing'''
+
+    print('\nFunction Table\n')
+    pp = pprint.PrettyPrinter(indent=2)
+    pp.pprint(symbol_table.func_table)
+
+    print('\nQuadruples\n')
+    quadruples = code_generator.quadruples
+    for q in quadruples:
+        print(q)
+
 
 success = True
 
@@ -9,15 +23,11 @@ archivo = "tests/test1.txt"
 f = open(archivo, 'r')
 s = f.read()
 
-parser = parser.parser
 parser.parse(s)
 
 if success == True:
-    print("Archivo aprobado")
-
-    pp = pprint.PrettyPrinter(indent=2)
-    pp.pprint(symbol_table.func_table)
-
+    print('Archivo aprobado')
+    testLog()
     sys.exit()
 else:
     print("Archivo no aprobado")
