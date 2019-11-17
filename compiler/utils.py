be9140405c52d9ef981760d66aa1c1931ff95a8b
Kevin Radtke <kevinradtkeg@gmail.com>
2019-11-17 00:24:34 -0600

    Fix: Missing imports and corrected var name

--- compiler/utils.py
+++ compiler/utils.py
@@ -1,5 +1,6 @@
 from symbol_table import Var
 import symbol_table
+import sys
 
 context = 'global'
 func_start_pos = 0
@@ -10,6 +11,6 @@ def id_lookup(id):
     elif (id in symbol_table.func_dir['global']['vars']):
         var = symbol_table.func_dir['global']['vars'][id]
     else:
-        print('ERROR: Variable with name `' + p[1] + '` does not exist!')
+        print('ERROR: Variable with name `' + id + '` does not exist!')
         sys.exit()
     return Var(var['type'], id, var['addr'])
