4d12f8206b9cdfdba70d041527012e2a6194f1a5
Eduardo Hernández <eddyhdzg@gmail.com>
2019-11-25 13:37:39 -0600

    app.py

---
+++ compiler/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, current_app
+from vm import flask_comp_and_run
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def index():
+    return 'INDEX3'
+
+
+@app.route('/compile', methods=["POST"])
+def run_api():
+
+    code = request.form['code']
+
+    file = open("copy.txt", "w")
+    file.write(code)
+    file.close()
+
+    flask_comp_and_run("copy.txt")
+
+    with current_app.open_resource('ide_output.txt') as f:
+        return f.read()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
